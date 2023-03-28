24287e765912e6f434ac6bb547e77d5fbf6b5a8b
Mila Nozella <nozellamila@gmail.com>
2023-03-28 19:23:50 -0300

    Configuração inicial producer kafka

--- src/main/java/com/producer/kafka/KafkaApplication.java
+++ src/main/java/com/producer/kafka/KafkaApplication.java
@@ -1,13 +1,23 @@
 package com.producer.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class KafkaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(KafkaApplication.class, args);
+
 	}
 
 }
